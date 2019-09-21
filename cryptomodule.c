f60aae8f41f9f497f1b086983085922bca0a90ad
Iago <iagojlourenco@gmail.com>
2019-09-20 21:02:57 -0300

    Added .gitignore, comecei a usar o vscode

--- cryptomodule.c
+++ cryptomodule.c
@@ -12,13 +12,17 @@
 #include <linux/device.h>   // Suporte para modulos de dispositivos
 #include <linux/fs.h>       // Suporte ao sistema de arquivos linux
 #include <linux/uaccess.h>  //Função copy_to_user
+#include <linux/crypto.h>   //Funçoes de criptografia
 
 #define DEVICE_NAME "crypto"    //Nome do dispositivo, aparece em /dev/crypto 
 #define CLASS_NAME "cryptomodule" 
-#define MODULE_VER "0.1"
+
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Adriano Munin, Fabio Irokawa, Iago Lourenço, Lucas Coutinho");
 MODULE_DESCRIPTION("Modulo de criptografia");
-MODULE_VERSION(MODULE_VER);
+MODULE_VERSION("0.1");
+
+
 
 
