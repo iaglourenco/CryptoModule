b9ebaaffb3f5f7c4202183a403d556737a407d42
lucasrcoutinho <33065527+lucasrcoutinho@users.noreply.github.com>
2019-10-17 01:24:06 -0300

    Padding/unpadding e hash implementados (Necessário mais testes)

--- cryptomodule.c
+++ cryptomodule.c
@@ -17,6 +17,7 @@
 #include <crypto/skcipher.h>   //Funçoes de criptografia
 #include <linux/mutex.h>
 #include <linux/scatterlist.h>
+#include <crypto/internal/hash.h>
 
 #define DEVICE_NAME "crypto"    //Nome do dispositivo, aparece em /dev/crypto 
 #define CLASS_NAME "cryptomodule" 
@@ -65,18 +66,16 @@ static int tamKey=0; //Para se lembrar do tamanho das strings
 //static char cryptoiv[32];
 
 static DEFINE_MUTEX(crypto_mutex);
-static char input[256]={0};
 static int tamInput;
 static char encrypted[256]={0};
 static int tamEncrypted; 
 static char decrypted[256]={0};
 static int tamDecrypted;
-static char hash[256]={0};
+static char hash[41]={0};
 static int tamHash;
 int pos,i;
 char op;
-char ascii[16]={0};
-char bloco[16]={0};
+//char ascii[16]={0};
 char buf;
 
 module_param(iv,charp,0000);
@@ -92,6 +91,9 @@ static ssize_t dev_read(struct file *,char *,size_t,loff_t * );
 static ssize_t dev_write(struct file *, const char *,size_t,loff_t *);
 static void init_cifra(char *msgInput, char *msgOutput, int opc);
 static void ascii2hexa(unsigned char *in, char *out, int len);
+static void unpadding(char *string, int len);
+static void padding(char *string, int len);
+static void init_hash(char *textIn, char *digest, int qtdChar);
 
 //Estrutura que define qual função chamar quando 
 //o dispositivo é requisitado
@@ -223,43 +225,85 @@ static ssize_t dev_read(struct file *filep,char *buffer,size_t len,loff_t *offse
 
 static ssize_t dev_write(struct file *filep,const char *buffer,size_t len, loff_t *offset){
     char temp[3];
-    int cont = 0;
+    char *ascii;
+    char input[256]={0};
+    char blocoIn[16]={0};
+    char blocoCrypto[16]={0};
+    int cont = 0, indice;
+
+
     strcpy(input, buffer+1);
     op = buffer[0];
-    tamInput = strlen(input); 
+    tamInput = strlen(input);
 
+    if(op == 'c') padding(input, tamInput); //Caso a opcao seja de criptgrafia, o padding eh feito na entrada.
+
+
+    ascii = kmalloc(tamInput/2 + 16, GFP_KERNEL);
+        if (!ascii) {
+                printk(KERN_ERR  "kmalloc(input) failed\n");
+                return -1;
+        }
+    tamInput = strlen(input);
     //Conversao de hexa para ascii
-    for(i = 0; i < tamInput; i+=2){
-        temp[0]  = input[i];
-        temp[1]  = input[i+1];
+    for(indice = 0; indice < tamInput; indice+=2){
+        temp[0]  = input[indice];
+        temp[1]  = input[indice+1];
         temp[2]  = '\0'; 
         sscanf(temp, "%hhx", &ascii[cont]);
         cont++;    
-    }
+    }    
 
     if(op == 'c'){
         printk("CRYPTO--> Criptografando..\n"); 
         //Aqui entra a criptografia!
-        init_cifra(ascii, bloco, 1);
-        ascii2hexa(bloco, encrypted, 16);       
+
+        for(indice = 0; indice < cont/16; indice++){//Cont tem a qtd de caracteres ascii, sempre multiplo de 16 (padding)            
+            for(i = 0; i < 16; i++){//Copia um bloco para criptografar
+                blocoIn[i] = ascii[indice*16 + i];//Indice*16 para deslocar o bloco (Indice tem o num. do bloco)
+            }
+
+            init_cifra(blocoIn, blocoCrypto, 1);
+            
+            for(i = 0; i < 16; i++){//Copia um bloco criptografado 
+                ascii[indice*16 +i] = blocoCrypto[i];//Como o bloco atual de ascii ja foi criptografado, ele eh sobrescrito
+            }
+        } 
+
+        ascii2hexa(ascii, encrypted, cont);//ascii tem todos os blocos criptografados
         tamEncrypted=strlen(encrypted);
         printk("DEBUG ASC2HEX %s\n",encrypted);
 
     }else if(op == 'd'){
         printk("CRYPTO--> Descriptografando..\n"); 
         //descriptografia aqui
-        init_cifra(ascii, bloco,  2);
-        ascii2hexa(bloco, decrypted, 16); 
+
+        for(indice = 0; indice < cont/16; indice++){//Cont tem a qtd de caracteres ascii, sempre multiplo de 16 (padding)            
+            for(i = 0; i < 16; i++){//Copia um bloco para criptografar
+                blocoIn[i] = ascii[indice*16 + i];//Indice*16 para deslocar o bloco (Indice tem o num. do bloco)
+            }
+
+            init_cifra(blocoIn, blocoCrypto, 2);
+            
+            for(i = 0; i < 16; i++){//Copia um bloco criptografado 
+                ascii[indice*16 +i] = blocoCrypto[i];//Como o bloco atual de ascii ja foi criptografado, ele eh sobrescrito
+            }
+        }
+
+        ascii2hexa(ascii, decrypted, cont);
         tamDecrypted=strlen(decrypted);
+        unpadding(decrypted, tamDecrypted);//Na descriptografia o unpadding eh feito na saida         
         printk("DEBUG HEX2ASC %s\n", decrypted);
     }else{
         printk("CRYPTO--> Gerando Hash..\n");
         //hash aqui
-        tamHash=strlen("Nao implementado ainda :(");
-        strcpy(hash,"Nao implementado ainda :(");
+        init_hash(ascii, encrypted, cont);
+        ascii2hexa(encrypted, hash, 40);
+        tamHash = 40;
     }
 
     printk(KERN_INFO "CRYPTO-->  Recebida mensagem com %d caracteres!\n",tamInput);
+    kfree(ascii);
     return len;
 }
 
@@ -349,6 +393,101 @@ out:
     if (req)
         skcipher_request_free(req);       
 }
+
+
+static void padding(char *string, int len){ //Padrao utilizado PKCS#7
+    int qdtBlocos32, bytesOcupados;
+    int i;
+    qdtBlocos32 = len/32;   //Obtem a quantidade de blocos completos
+    bytesOcupados = len%32; //Obtem a quantidade de bytes usados no ultimo bloco
+
+    if(bytesOcupados == 0){ //Caso a string tenha o tamanho multiplo de 16, preenche um novo blco com o num 0x20 (tamanho do bloco)
+        for(i = 0; i < 32; i++){
+            sprintf(string+qdtBlocos32*32+ i*2,"%02x", 32);//Converte 32 decimal para hexa (0x20)
+        }
+        string[qdtBlocos32*32 + 32] = '\0';
+    }
+
+    else {
+        for(i = 0; i < 32 - bytesOcupados; i++){//O ultimo bloco eh preenchido com o valor da qtd de bytes livres
+            sprintf(string + qdtBlocos32*32 + bytesOcupados + i*2,"%02x", 32 - bytesOcupados);
+         }
+        string[qdtBlocos32*32 + 32] = '\0';
+    }
+    
+}
+
+static void unpadding(char *string, int len){ //Padrao utilizado PKCS#7
+    char temp[3];
+    int qtdPadding;//Quantidade de bytes usados no padding
+    int numP;//Numero usado para preencher o padding
+    int i;
+
+    temp[0]  = string[len-2];//Ultimo numero sempre eh usado para calcular o padding
+    temp[1]  = string[len-1];
+    temp[2]  = '\0';    
+    sscanf(temp, "%x", &qtdPadding);// Converte o num de hexa para decimal
+
+    printk("qtdPadding: %i\n", qtdPadding);
+
+    for(i = 0; i < qtdPadding; i += 2){
+        temp[0]  = string[len - 2 - i];
+        temp[1]  = string[len - 1 - i];
+        temp[2]  = '\0';
+        sscanf(temp, "%x", &numP);
+        if(numP != qtdPadding){//Caso o numero usado para preencher seja diferente da qtd, retorna erro
+            printk("Erro de padding\n");
+            return; 
+        } 
+    }
+    string[len - qtdPadding] = '\0';//Descarta numeros usados no padding
+}
+
+
+static void init_hash(char *textIn, char *digest, int qtdChar){
+    struct crypto_shash *sha1;
+    struct shash_desc *shash;
+    int ret;
+
+    sha1 = crypto_alloc_shash("sha1", 0, 0);
+    if (IS_ERR(sha1)){
+        printk(KERN_ERR  "hash failed erro: nao foi possivel alocar shash");
+        return;
+    }
+
+    shash = kmalloc(40 ,GFP_KERNEL);
+    if (!shash){
+        printk(KERN_ERR  "hash failed erro: %i\n", ENOMEM);
+        return;
+    }
+
+    shash->tfm = sha1;
+    shash->flags = 0;
+
+    ret = crypto_shash_init(shash);
+    if (ret){
+        printk(KERN_ERR  "hash failed erro: %i\n", ret);
+        return;
+    }        
+        
+    ret = crypto_shash_update(shash, textIn, qtdChar);
+    if (ret){
+        printk(KERN_ERR  "hash failed erro: %i\n", ret);
+        return;
+    }        
+        
+    ret = crypto_shash_final(shash, digest);
+
+    if (ret){
+        printk(KERN_ERR  "hash failed erro: %i\n", ret);
+        return;
+    }  
+        
+
+    kfree(shash);
+    crypto_free_shash(sha1);
+}
+
 
 module_init(crypto_init);
 module_exit(crypto_exit);
